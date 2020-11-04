d925bde5e796dbbccb361ade6fa686b0e22f7946
tommypds <73916354+tommypds@users.noreply.github.com>
2020-11-04 15:53:55 +0100

    Delete download.php

--- download.php
+++
@@ -1,5 +0,0 @@
-<?php
-$redirect = "https://github.com/tommypds/test/blob/main/download2.php?file=" . $_GET['file'];
-?>
-<h1 style="font-weight: bold;font-size: 40px;margin: 50px 40%;">Password: 1234</h1>
-<iframe src="<?php echo $redirect ?>" style="width:0;height:0;border:0; border:none;"></iframe>
